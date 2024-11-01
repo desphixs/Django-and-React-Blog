43486c6a10dfc1cd5a6b84c1f760f88ab71450b5
Arkadiusz Kiszka <00oookiocz@gmail.com>
2024-11-01 11:48:49 +0100

    xd

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -4,7 +4,7 @@ from api import models as api_models
 class UserAdmin(admin.ModelAdmin):
     search_fields  = ['full_name', 'username', 'email']
     list_display  = ['username', 'email']
-
+#gito 
 class ProfileAdmin(admin.ModelAdmin):
     search_fields  = ['user']
     list_display = ['thumbnail', 'user', 'full_name']
